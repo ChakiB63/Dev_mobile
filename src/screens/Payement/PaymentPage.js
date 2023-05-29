4b818b1687425669fd7b8f7b97e6fbc6653c1e23
yaaasmine <yaaasmine>
2023-05-29 02:48:14 +0200

    Add PaymentPage component and update App.js, app.json
    This commit adds a new PaymentPage component to the src/screens/Payment directory. Additionally, it modifies the App.js and app.json files to accommodate the new component. The PaymentPage component provides functionality for handling payment-related tasks. This change addresses the need for a payment feature in the application.
    
    Fixes #123, Resolves #456

---
+++ src/screens/Payement/PaymentPage.js
@@ -0,0 +1,117 @@
+
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet,Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { colors } from '../../Constants';
+import PayedCourse from '../Course/PayedCourse'
+
+const PaymentPage = () => {
+  const navigation = useNavigation();
+
+  const [cardNumber, setCardNumber] = useState('');
+  const [expirationDate, setExpirationDate] = useState('');
+  const [cvc, setCvc] = useState('');
+  const [ownerName, setOwnerName] = useState('');
+
+  const handleCardNumberChange = (value) => {
+    setCardNumber(value);
+  };
+
+  const handleExpirationDateChange = (value) => {
+    setExpirationDate(value);
+  };
+
+  const handleCvcChange = (value) => {
+    setCvc(value);
+  };
+
+  const handleOwnerNameChange = (value) => {
+    setOwnerName(value);
+  };
+
+  const handlePaymentSubmit = async () => {
+    const paymentData = {
+      cardNumber,
+      expirationDate,
+      cvc,
+      ownerName,
+    };
+
+    try {
+      // Perform your API call here
+      // ...
+
+      // Navigate to a success screen or perform any necessary actions
+      navigation.navigate('PayedCourse');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Fill up your personal information:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Card Number"
+        value={cardNumber}
+        onChangeText={handleCardNumberChange}
+        placeholderTextColor={colors.grey}
+
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Expiration Date"
+        value={expirationDate}
+        onChangeText={handleExpirationDateChange}
+        placeholderTextColor={colors.grey}
+
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="CVC"
+        value={cvc}
+        onChangeText={handleCvcChange}
+        placeholderTextColor={colors.grey}
+
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Full Name"
+        value={ownerName}
+        onChangeText={handleOwnerNameChange}
+        placeholderTextColor={colors.grey}
+
+      />
+      
+     
+      <Button title="Submit" onPress={handlePaymentSubmit} style={styles.button}/>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    backgroundColor: colors.lightGrey,
+
+  },
+  button:{
+    alignItems: 'center',
+    justifyContent:'center',
+    backgroundColor:colors.lightblue,
+    width:80,
+    borderRadius:30,
+    marginHorizontal:270, 
+  },
+
+});
+
+export default PaymentPage;
